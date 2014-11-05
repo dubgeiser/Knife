7f877329a0c28f1301ec6a6f356d88113b618fd2
Per <dubgeiser@gmail.com>
2014-11-05 18:49:29 +0100

    Automate testing installing Knife via Composer.

---
+++ Tests/System/ComposerInstallTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Knife\Tests\System;
+
+
+/**
+ * Clean install Knife via composer and check if it runs.
+ */
+class ComposerInstallTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @var string The directory at the time this test runs.
+	 */
+    private $cwd;
+
+    public function setUp()
+    {
+        $this->cwd = getcwd();
+        chdir(__DIR__ . '/../assets/install');
+		shell_exec('rm -fr vendor/ composer.lock');
+		shell_exec('composer install');
+    }
+
+    public function tearDown()
+    {
+		shell_exec('rm -fr vendor/ composer.lock');
+		chdir($this->cwd);
+    }
+
+    public function testShouldBeInstallableViaComposer()
+    {
+		$output = shell_exec('php test.php');
+		$this->assertContains('123', $output);
+    }
+}
+
